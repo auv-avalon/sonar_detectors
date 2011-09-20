56c0f1dc197ea7246dbf80cd82b8f335c90fb09b
Sascha Arnold <sascha.arnold@dfki.de>
2011-09-20 20:52:10 +0200

    added a seperated visualization for the avalon model, witch also can display the desired position of avalon.

---
+++ viz/AUVAvalonVisualization.cpp
@@ -0,0 +1,165 @@
+#include "AUVAvalonVisualization.hpp"
+#include <osgDB/ReadFile>
+#include <avalonmath.h>
+#include <osg/ShapeDrawable>
+
+namespace vizkit
+{
+
+AUVAvalonVisualization::AUVAvalonVisualization()
+{
+    VizPluginRubyAdapter(AUVAvalonVisualization, base::samples::RigidBodyState, RigidBodyState)
+    VizPluginRubyAdapter(AUVAvalonVisualization, base::AUVPositionCommand, DesiredPosition)
+    VizPluginRubyMethod(AUVAvalonVisualization, bool, showDesiredModelPosition)
+    VizPluginRubyMethod(AUVAvalonVisualization, bool, desiredModelPositionRelZ)
+    VizPluginRubyMethod(AUVAvalonVisualization, bool, desiredModelPositionRelHeading)
+    rbs.invalidate();
+    showDesiredModel = false;
+    desiredModel_rel_z = false;
+    desiredModel_rel_heading = true;
+    newPositionCommand = false;
+}
+
+osg::ref_ptr< osg::Node > AUVAvalonVisualization::createMainNode()
+{
+    osg::ref_ptr<osg::Group> mainNode = new osg::Group();
+    
+    avalonModelPos = new osg::PositionAttitudeTransform();
+    osg::ref_ptr<osg::PositionAttitudeTransform> avalonPosAdjustment = new osg::PositionAttitudeTransform();
+    mainNode->addChild(avalonModelPos);
+    avalonModelPos->addChild(avalonPosAdjustment);
+    
+    // get avalon model
+    char* osgPath = getenv("OSG_FILE_PATH");
+    if (osgPath) 
+    {
+        std::string filePath(osgPath);
+        filePath += "/avalon.osg";
+        avalonModel = osgDB::readNodeFile(filePath);
+    }
+    if (avalonModel.get() == 0)
+    {
+        avalonModel = printPrimitivModel();
+    }
+    
+    // adjust position of the avalon model
+    avalonPosAdjustment->addChild(avalonModel);
+    base::Quaterniond quat = Avalonmath::eulerToQuaternion(-M_PI / 2.0, 0.0, M_PI / 2.0);
+    osg::Quat orientation = osg::Quat(quat.x(), quat.y(), quat.z(), quat.w());
+    avalonPosAdjustment->setAttitude(orientation);
+    avalonPosAdjustment->setPosition(osg::Vec3d(0.7,0,0));
+    
+    // print desired avalon model position
+    desiredModelPos = new osg::PositionAttitudeTransform();
+    if(showDesiredModel)
+    {
+        osg::ref_ptr<osg::PositionAttitudeTransform> desiredPosAdjustment = new osg::PositionAttitudeTransform();
+        avalonModelPos->addChild(desiredModelPos);
+        desiredModelPos->addChild(desiredPosAdjustment);
+        
+        osg::ref_ptr<osg::Node> desiredModel = printPrimitivModel(0.3);
+        desiredPosAdjustment->addChild(desiredModel);
+        
+        desiredPosAdjustment->setAttitude(orientation);
+        desiredPosAdjustment->setPosition(osg::Vec3d(0.7,0,0));
+    }
+    
+    return mainNode;
+}
+
+/**
+ * This will print a primitive model if the common avalon model wasn't found.
+ * 
+ * @return primitiv avalon model
+ */
+osg::ref_ptr< osg::Node > AUVAvalonVisualization::printPrimitivModel(float alpha)
+{
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+    float height = 1.4;
+    float radius = 0.1;
+    osg::Vec4 color_cylinder(0.78f, 0.59f, 0.25f, alpha);
+    osg::Vec4 color_window(0.70f, 0.71f, 0.87f, alpha);
+    osg::ref_ptr<osg::ShapeDrawable> shape1 = new osg::ShapeDrawable(new osg::Cylinder(osg::Vec3f(0.0,0.0,height/2.0), radius, height));
+    shape1->setColor(color_cylinder);
+    osg::ref_ptr<osg::ShapeDrawable> shape2 = new osg::ShapeDrawable(new osg::Sphere(osg::Vec3f(0.0,0.0,0.0), radius));
+    shape2->setColor(color_window);
+    geode->addDrawable(shape1);
+    geode->addDrawable(shape2);
+    return geode;
+}
+
+/**
+ * @return plugin name
+ */
+const std::string AUVAvalonVisualization::getPluginName() const
+{
+    return "AUVAvalonVisualization";
+}
+
+void AUVAvalonVisualization::showDesiredModelPosition(bool b)
+{
+    showDesiredModel = b;
+}
+
+void AUVAvalonVisualization::desiredModelPositionRelZ(bool b)
+{
+    desiredModel_rel_z = b;
+}
+
+void AUVAvalonVisualization::desiredModelPositionRelHeading(bool b)
+{
+    desiredModel_rel_heading = b;
+}
+
+/**
+ * Updates the class with the new body state of type RigidBodyState.
+ * 
+ * @param data new body state data
+ */
+void AUVAvalonVisualization::updateDataIntern(const base::samples::RigidBodyState& data)
+{
+    rbs = data;
+}
+
+/**
+ * Updates the class with a new position command.
+ * 
+ * @param data new position command data
+ */
+void AUVAvalonVisualization::updateDataIntern(const base::AUVPositionCommand& data)
+{
+    positionCommand = data;
+    newPositionCommand = true;
+}
+
+void AUVAvalonVisualization::updateMainNode(osg::Node* node)
+{
+    //pos.set(rbs.position.x(), rbs.position.y(), rbs.position.z());
+    //avalonModelPos->setPosition(pos);
+    orientation.set(rbs.orientation.x(), rbs.orientation.y(), rbs.orientation.z(), rbs.orientation.w());
+    avalonModelPos->setAttitude(orientation);
+    
+    if(showDesiredModel && newPositionCommand)
+    {
+        newPositionCommand = false;
+        osg::Vec3d desiredPos(positionCommand.x, positionCommand.y, 
+                       (desiredModel_rel_z) ? positionCommand.z : positionCommand.z - rbs.position.z());
+        desiredModelPos->setPosition(desiredPos);
+        osg::Quat desiredOri(rbs.orientation.x(), rbs.orientation.y(), rbs.orientation.z(), rbs.orientation.w());
+        osg::Vec3d top = desiredOri * osg::Vec3d(0,0,1);
+        if(desiredModel_rel_heading)
+        {
+            desiredOri.makeRotate(positionCommand.heading, top.x(), top.y(), top.z());
+        }
+        else
+        {
+            double angle;
+            desiredOri.getRotate(angle, top.x(), top.y(), top.z());
+            desiredOri.makeRotate(positionCommand.heading - angle, top.x(), top.y(), top.z());
+        }
+        desiredModelPos->setAttitude(desiredOri);
+    }
+}
+
+VizkitQtPlugin(AUVAvalonVisualization)
+}
---
+++ viz/AUVAvalonVisualization.hpp
@@ -0,0 +1,47 @@
+#ifndef AUVAvalonVisualization_H
+#define AUVAvalonVisualization_H
+
+#include <vizkit/Vizkit3DPlugin.hpp>
+#include <base/samples/rigid_body_state.h>
+#include <base/motion_command.h>
+
+#include <osg/Node>
+#include <osg/PositionAttitudeTransform>
+
+namespace vizkit
+{
+    
+class AUVAvalonVisualization : public vizkit::Vizkit3DPlugin<base::samples::RigidBodyState>,
+                               public vizkit::VizPluginAddType<base::AUVPositionCommand>
+{
+public:
+    AUVAvalonVisualization();
+    const std::string getPluginName() const;
+    
+protected:
+    virtual osg::ref_ptr<osg::Node> createMainNode();
+    virtual void updateMainNode( osg::Node* node );
+    void updateDataIntern ( const base::samples::RigidBodyState& data );
+    void updateDataIntern ( const base::AUVPositionCommand& data );
+    void showDesiredModelPosition(bool b);
+    void desiredModelPositionRelZ(bool b);
+    void desiredModelPositionRelHeading(bool b);
+    
+private:
+    osg::ref_ptr< osg::Node > printPrimitivModel(float alpha = 1.0);
+    
+    base::samples::RigidBodyState rbs;
+    base::AUVPositionCommand positionCommand;
+    osg::ref_ptr<osg::PositionAttitudeTransform> avalonModelPos;
+    osg::ref_ptr<osg::PositionAttitudeTransform> desiredModelPos;
+    osg::ref_ptr<osg::Node> avalonModel;
+    osg::Quat orientation;
+    osg::Vec3d pos;
+    bool showDesiredModel;
+    bool desiredModel_rel_z;
+    bool desiredModel_rel_heading;
+    bool newPositionCommand;
+};
+   
+}
+#endif
