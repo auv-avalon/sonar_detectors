91b5b5700355f834920e751b793f93e3026afee5
Sascha Arnold <sascha.arnold@dfki.de>
2011-10-20 01:14:22 +0200

    added class SonarEnvironmentModel

--- src/FeatureExtraction.hpp
+++ src/FeatureExtraction.hpp
@@ -3,9 +3,44 @@
 
 #include <vector>
 #include <stdint.h>
+#include <math.h>
+#include <base/eigen.h>
+#include <map>
+
+#define M_PI_8 (M_PI_4 * 0.5)
 
 namespace sonar_detectors
 {
+    
+class SonarEnvironmentModel
+{
+public:
+    SonarEnvironmentModel();
+    
+    void getExpectedObstaclePositions(const double beam_angle, int &pos_auv, int &pos_ground, int &pos_surface);
+    
+    void setAUVOrientation(const base::Quaterniond orientation);
+    void setDistanceToGround(const double distance);
+    void setDistanceToSurface(const double distance);
+    void setSonarBeamProperties(const double sampling_interval, const float beamwidth_vertical = (35.0/180.0)*M_PI, const float speed_of_sound = 1500);
+    
+private:
+    int getExpectedObstaclePosition(const double beamwidth_vertical_angle, const double distance_to_plain);
+    int getAUVModelConstrains(const double beam_angle, double &beamwidth_vertical_down);
+    
+    std::vector<base::Vector2d> avalon_model;
+    std::map<double, double> angle_to_edge_distance;
+    
+    double distance_to_ground;
+    double distance_to_surface;
+    double distance_to_avalon_model;
+    
+    base::Quaterniond orientation;
+    double sampling_interval;
+    float beamwidth_vertical;
+    float speed_of_sound;
+    
+};
 
 class FeatureExtraction
 {
