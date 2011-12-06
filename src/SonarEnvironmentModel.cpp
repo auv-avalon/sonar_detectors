48b68419eb2550720e945bda5de60426e32338ee
Sascha Arnold <sascha.arnold@dfki.de>
2011-12-06 17:17:32 +0100

    added noise estimation distributions to the sonar environment model

--- src/SonarEnvironmentModel.cpp
+++ src/SonarEnvironmentModel.cpp
@@ -1,7 +1,10 @@
 #include "SonarEnvironmentModel.hpp"
+#include <dsp_acoustics/FIRFilter.h>
 
 namespace sonar_detectors
 {
+    
+const static float sqrt_2_PI = sqrt(2.0f*M_PI);
     
 SonarEnvironmentModel::SonarEnvironmentModel() : 
                         orientation(base::Quaterniond::Identity()),
@@ -12,11 +15,70 @@ SonarEnvironmentModel::SonarEnvironmentModel() :
                         distance_to_surface(0),
                         distance_to_avalon_model(0.15)
 {
+    // abstract plain model of avalon
     avalon_model.push_back(base::Vector2d(0, 0.4));
     avalon_model.push_back(base::Vector2d(M_PI_4, 0.35));
     avalon_model.push_back(base::Vector2d(M_PI_2, 0.15));
     avalon_model.push_back(base::Vector2d(7 * M_PI_8, 1.05));
     avalon_model.push_back(base::Vector2d(M_PI, 1.10));
+    
+    // noise distributions variables
+    device_noise_first_local_min_pos = 0;
+    device_noise_sigma = 1.0f;
+    gaussian_surface_sigma = 1.0f;
+    gaussian_surface_u = 0.0f;
+    gaussian_surface_k = 1.0f;
+    gaussian_ground_sigma = 1.0f;
+    gaussian_ground_u = 0.0f;
+    gaussian_ground_k = 1.0f;
+    // noise distributions config
+    find_first_min_windowsize = 6;
+    device_noise_minpos_update_rate = 0.05f;
+    device_noise_grow_update_rate = 0.01f;
+    gaussian_surface_update_rate = 0.01f;
+    gaussian_ground_update_rate = 0.001f;
+}
+
+void SonarEnvironmentModel::updateNoiseDistributionValues(const double beam_angle, const std::vector< float > &beam)
+{
+    int pos_auv, pos_ground, pos_surface;
+    getExpectedObstaclePositions(beam_angle, pos_auv, pos_ground, pos_surface);
+
+    // correct device noise sigma
+    std::vector<float>::const_iterator device_noise_new_local_min = dsp::findFirstRightLocalMin<std::vector<float>::const_iterator, std::vector<float>::iterator>(beam.begin(), beam.end(), find_first_min_windowsize);
+    if(device_noise_new_local_min != beam.begin())
+    {
+        device_noise_first_local_min_pos = device_noise_first_local_min_pos * (1-device_noise_minpos_update_rate) + (device_noise_new_local_min - beam.begin()) * device_noise_minpos_update_rate;
+        if(device_noise_first_local_min_pos < pos_auv)
+            device_noise_first_local_min_pos = pos_auv;
+        float diff = beam[device_noise_first_local_min_pos] - device_noise_distribution(device_noise_first_local_min_pos);
+        device_noise_sigma += (device_noise_grow_update_rate * diff);
+        if(device_noise_sigma < 1.0f) device_noise_sigma = 1.0f;
+    }
+    
+    // correct surface gaussian sigma
+    gaussian_surface_u = (float)pos_surface;
+    std::vector<float>::const_iterator gaussian_surface_local_min = dsp::findFirstRightLocalMin<std::vector<float>::const_iterator, std::vector<float>::iterator>(beam.begin() + pos_surface, beam.end(), find_first_min_windowsize);
+    float diff_surface = *gaussian_surface_local_min - gaussian_distribution_surface(gaussian_surface_local_min - beam.begin());
+    gaussian_surface_sigma += (gaussian_surface_update_rate * diff_surface);
+    if(gaussian_surface_sigma < 1.0f) gaussian_surface_sigma = 1.0f;
+    // correct surface gaussian k
+    diff_surface = beam[pos_surface] - gaussian_distribution_surface(pos_surface);
+    gaussian_surface_k += (gaussian_surface_update_rate * diff_surface);
+    if(gaussian_surface_k < 1.0f) gaussian_surface_k = 1.0f;
+    //TODO improve the relation between height and width of the gauss distributions
+    //else if (gaussian_surface_k > gaussian_surface_sigma) gaussian_surface_k = gaussian_surface_sigma;
+    
+    // correct ground gaussian sigma
+    gaussian_ground_u = (float)pos_ground;
+    std::vector<float>::const_iterator gaussian_ground_local_min = dsp::findFirstRightLocalMin<std::vector<float>::const_iterator, std::vector<float>::iterator>(beam.begin() + pos_ground, beam.end(), find_first_min_windowsize);
+    float diff_ground = *gaussian_ground_local_min - gaussian_distribution_ground(gaussian_ground_local_min - beam.begin());
+    gaussian_ground_sigma += (gaussian_ground_update_rate * diff_ground);
+    if(gaussian_ground_sigma < 1.0f) gaussian_ground_sigma = 1.0f;
+    // correct ground gaussian k
+    diff_ground = beam[pos_ground] - gaussian_distribution_ground(pos_ground);
+    gaussian_ground_k += (gaussian_ground_update_rate * diff_ground);
+    if(gaussian_ground_k < 1.0f) gaussian_ground_k = 1.0f;
 }
 
 void SonarEnvironmentModel::getExpectedObstaclePositions(const double beam_angle, int& pos_auv, int& pos_ground, int& pos_surface)
@@ -112,6 +174,26 @@ void SonarEnvironmentModel::updateDistanceToSurface(const double distance)
         distance_to_surface = 0.0;
     else 
         distance_to_surface = distance;
+}
+
+float SonarEnvironmentModel::device_noise_distribution(float x)
+{
+    return 255.0f * exp(-x/device_noise_sigma);
+}
+
+float SonarEnvironmentModel::gaussian_distribution(const float &x, const float &sigma, const float &mu, const float &k)
+{
+    return (k*100.0f/(sigma * sqrt_2_PI)) * exp(-0.5f*(pow((x-mu) / sigma, 2)));
+}
+
+float SonarEnvironmentModel::gaussian_distribution_ground(float x)
+{
+    return gaussian_distribution(x, gaussian_ground_sigma, gaussian_ground_u, gaussian_ground_k);
+}
+
+float SonarEnvironmentModel::gaussian_distribution_surface(float x)
+{
+    return gaussian_distribution(x, gaussian_surface_sigma, gaussian_surface_u, gaussian_surface_k);
 }
     
 }
--- src/SonarEnvironmentModel.hpp
+++ src/SonarEnvironmentModel.hpp
@@ -11,21 +11,37 @@
 namespace sonar_detectors
 {
     
+//noise distributions variables
+static float gaussian_ground_sigma;
+static float gaussian_ground_u;
+static float gaussian_ground_k;
+static float gaussian_surface_sigma;
+static float gaussian_surface_u;
+static float gaussian_surface_k;
+static float device_noise_sigma;
+    
 class SonarEnvironmentModel
 {
 public:
     SonarEnvironmentModel();
     
+    void updateNoiseDistributionValues(const double beam_angle, const std::vector<float> &beam);
     void getExpectedObstaclePositions(const double beam_angle, int &pos_auv, int &pos_ground, int &pos_surface);
-    
     void updateAUVOrientation(const base::Quaterniond orientation);
     void updateDistanceToGround(const double distance);
     void updateDistanceToSurface(const double distance);
     void updateSonarBeamProperties(const double sampling_interval, const float beamwidth_vertical = (35.0/180.0)*M_PI, const float speed_of_sound = 1500.0);
     
+    //noise distributions
+    static float gaussian_distribution_ground(float x);
+    static float gaussian_distribution_surface(float x);
+    static float device_noise_distribution(float x);
+    
+    
 private:
     int getExpectedObstaclePosition(const double beamwidth_vertical_angle, const double distance_to_plain);
     int getAUVModelConstrains(const double beam_angle, double &beamwidth_vertical_down);
+    static float gaussian_distribution(const float &x, const float &sigma, const float &mu, const float &k);
     
     std::vector<base::Vector2d> avalon_model;
     std::map<double, double> angle_to_edge_distance;
@@ -37,6 +53,13 @@ private:
     double sampling_interval;
     float beamwidth_vertical;
     float speed_of_sound;
+    
+    unsigned int find_first_min_windowsize;
+    int device_noise_first_local_min_pos;
+    float device_noise_minpos_update_rate;
+    float device_noise_grow_update_rate;
+    float gaussian_surface_update_rate;
+    float gaussian_ground_update_rate;
 };
     
 }
