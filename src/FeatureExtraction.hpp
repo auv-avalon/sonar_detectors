b351893711fa4c88894017c48f2cfdf0e0c0a1ad
Sascha Arnold <sascha.arnold@dfki.de>
2011-12-20 22:35:53 +0100

    added comments

--- src/FeatureExtraction.hpp
+++ src/FeatureExtraction.hpp
@@ -14,16 +14,60 @@ public:
     FeatureExtraction();
     ~FeatureExtraction();
     
+    /** ## Old feature detectors. Use 'setBoundingBox' and 'setMinResponseValue' for configuration ## **/
+    
+    /**
+     * Detects the global maxima in a signal. First the maximum sliding window, 
+     * than the maximum peak in that window.
+     * @param beam the sonar signal
+     * @param indexWindowSize size of the sliding window
+     * @return index of the peak
+     */
     int getFeatureGlobalMaxima(const std::vector<float>& beam, const unsigned int &indexWindowSize = 20);
+    /**
+     * Detects the maximum negative difference between two sliding windows.
+     * @param beam the sonar signal
+     * @param indexWindowSize size of the sliding windows
+     * @return index of the peak
+     */
     int getFeatureMaximalLevelDifference(const std::vector<float>& beam, const unsigned int &indexWindowSize = 20);
+    /**
+     * Sets a radial bounding box.
+     * @param radius of the bounding box
+     * @param sampling_interval of the sonar beam
+     * @param speed_of_sound in water
+     */
+    void setBoundingBox(const double radius, const double sampling_interval, const int speed_of_sound = 1500);
+    /**
+     * Sets a threshold for the sonar signals.
+     * @param minValue threshold
+     */
+    void setMinResponseValue(const double minValue);
+    
+    
+    /** ## New feature detector. Use 'featureDerivativeHistoryConfiguration' for configuration ## **/
+    
+    /**
+     * Returns the spot of the maximum accumulation in the history-derivative. 
+     * The history-derivative is the minimum of the last |derivative_history_length| derivatives.
+     * @param beam the sonar signal
+     * @return index of the peak
+     */
     int getFeatureDerivativeHistory(const std::vector<float>& beam);
+    /**
+     * Configuration for the featureDerivativeHistory.
+     * @param derivative_history_length num of the derivatives to build the history
+     * @param feature_threshold threshold for new features in percent of the mean of the last |best_values_size| best features
+     * @param best_values_size describes how many best-features will be saved to build the threshold
+     * @param signal_balancing signal_balancing is a slope that corrects the signal moderation
+     * @param plain_length the length of the plain in percent of signal length, if it is 0 the plain will be ignored
+     * @param plain_threshold the threshold to be a acceptable plain in percent of the mean signal strength
+     */
     void featureDerivativeHistoryConfiguration(const unsigned int &derivative_history_length, const float &feature_threshold, const unsigned int &best_values_size, 
         const float &signal_balancing, const float &plain_length, const float &plain_threshold);
     
     
     std::vector<float> convertBeam(const std::vector<uint8_t>& beam);
-    void setBoundingBox(const double radius, const double sampling_interval, const int speed_of_sound = 1500);
-    void setMinResponseValue(const double minValue);
     
 protected:
     void addToDerivativeHistory(const std::vector<float>& beam, const unsigned int &history_length);
